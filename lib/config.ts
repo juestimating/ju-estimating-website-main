0c36f3215b6ed7048572fee72807d3452b6cc8d0
IamHamadMalik <hamadmalik05@gmail.com>
2025-08-26 21:13:51 +0500

    changes

--- lib/config.ts
+++ lib/config.ts
@@ -3,18 +3,9 @@ export const siteConfig = {
   phoneDisplay: "(703) 721-8864",
   email: "info@juestimating.com",
   address: {
-    primary: {
-      street: "6545 MARKET AVE. NORTH",
-      suite: "",
-      city: "NORTH CANTON OH 44721",
-      full: "6545 MARKET AVE. NORTH NORTH CANTON OH 44721",
-    },
-    secondary: {
-      street: "8855 Peregrine Heights Rd",
-      suite: "Apt 201",
-      city: "Manassas, VA 20111",
-      full: "8855 Peregrine Heights Rd Apt 201 Manassas, VA 20111",
-    },
+    street: "6545 Market Ave. North",
+    city: "North Canton, OH 44721",
+    full: "6545 Market Ave. North, North Canton, OH 44721",
   },
   company: {
     name: "JU Estimating",
